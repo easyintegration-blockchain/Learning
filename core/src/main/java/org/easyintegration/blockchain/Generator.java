550de554e86c29ed78a8e102c771f8de2001fffe
easyintegration-blockchain <easyintegration.blockchain@gmail.com>
2022-07-01 11:09:57 +0000

    Initial Draft

--- core/src/main/java/org/easyintegration/blockchain/Generator.java
+++ core/src/main/java/org/easyintegration/blockchain/Generator.java
@@ -1,5 +1,8 @@
 package org.easyintegration.blockchain
 
+@ApplicationScoped
 public class Generator{
-
+    void onStart(@Observes StartupEvent ev) {
+        System.out.println("HelloWorld");
+    }
 }

620669257aa9d51e45265c8ebd9f18f3ed9cefe1
easyintegration-blockchain <easyintegration.blockchain@gmail.com>
2022-07-01 12:51:42 +0000

    Initial Draft

--- scripts/02-core.sh
+++ scripts/02-core.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+## Core Api Project
 quarkus create app org.easyintegration.blockchain:core \
 --extension=quarkus-arc \
 --extension=quarkus-vertx \
 --extension=camel-quarkus-core \
 --extension=camel-quarkus-kamelet \
 --extension=camel-quarkus-yaml-dsl 
+
+## Core Web Project
+cd core/src/main/webapp
+npx create-react-app . --template typescript
+npm install @patternfly/patternfly --save

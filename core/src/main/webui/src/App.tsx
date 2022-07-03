d1680a23e873167010a21b26491074f6391a5ced
easyintegration-blockchain <easyintegration.blockchain@gmail.com>
2022-07-03 07:55:14 +0000

    Initial Draft

--- core/src/main/webui/src/App.tsx
+++ core/src/main/webui/src/App.tsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
           Learn React
       </header>
     </div>

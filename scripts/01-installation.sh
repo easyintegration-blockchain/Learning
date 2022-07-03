b68493006b0b7e5a69b05e462fe7eba38700b295
easyintegration-blockchain <easyintegration.blockchain@gmail.com>
2022-07-03 06:43:40 +0000

    Initial Draft

--- scripts/01-installation.sh
+++ scripts/01-installation.sh
@@ -3,6 +3,7 @@
 ## Java /home/gitpod/.sdkman/candidates/java/11.0.15.fx-zulu
 ## Install 
 sudo apt-get install dos2unix -y
+sudo apt-get install rsync -y
 
 ## Jbang Installation
 curl -Ls https://sh.jbang.dev | bash -s - app setup
